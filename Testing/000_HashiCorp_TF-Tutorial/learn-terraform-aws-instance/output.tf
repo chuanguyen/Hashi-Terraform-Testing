ec0c75c94a70848bcba47d2691bae00a4a35b6ef
Calvin Hua-Nguyen <chua-services+github@protonmail.com>
2022-03-26 07:06:54 -0700

    Add output to AWS Terraform testing

---
+++ Testing/000_HashiCorp_TF-Tutorial/learn-terraform-aws-instance/output.tf
@@ -0,0 +1,10 @@
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = "AWS Instance ID: ${aws_instance.app_server.id}"
+  sensitive   = true
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
