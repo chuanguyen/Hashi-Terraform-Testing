e0a6839f563cb1b3c896c1f0a7801ba308a140b0
Calvin Hua-Nguyen <github.vcs.code@m.chuanguyen.ca>
2024-10-02 12:01:08 -0700

    Grant AWs access for Github Action

--- Home-Lab/prod/aws/main.tf
+++ Home-Lab/prod/aws/main.tf
@@ -10,3 +10,49 @@ terraform {
     path = "./terraform.tfstate"
   }
 }
+
+resource "aws_iam_openid_connect_provider" "oidc_github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+  thumbprint_list = ["D89E3BD43D5D909B47A18977AA9D5CE36CEE184C"]
+}
+
+# Create role for Github OIDC provider
+
+data "aws_iam_policy_document" "trustPolicyforGitHubOIDC" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.oidc_github.arn]
+    }
+
+    actions = [
+      "sts:AssumeRoleWithWebIdentity",
+    ]
+
+    condition {
+        test = "ForAnyValue:StringEquals"
+        variable = "token.actions.githubusercontent.com:sub"
+        values = ["repo:chuanguyen/AWS-Projects:ref:refs/heads/main"]
+    }
+
+    condition {
+        test = "ForAnyValue:StringEquals"
+        variable = "token.actions.githubusercontent.com:aud"
+        values = ["sts.amazonaws.com"]
+    }
+
+  }
+}
+
+# Assume Role Policy is separate from the other policies I created
+resource "aws_iam_role" "GitHubAction-AssumeRoleWithAction" {
+  name               = "GitHubAction-AssumeRoleWithAction"
+  assume_role_policy = data.aws_iam_policy_document.trustPolicyforGitHubOIDC.json
+}
