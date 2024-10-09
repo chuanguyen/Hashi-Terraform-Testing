bb6b2b25c5b62b8a84cfe019f267e299645bbf21
Calvin Hua-Nguyen <github.vcs.code@m.chuanguyen.ca>
2024-10-09 10:37:29 -0700

    Add feature flag for testing

--- Home-Lab/prod/aws/lambda_api_gateway_integrations.tf
+++ Home-Lab/prod/aws/lambda_api_gateway_integrations.tf
@@ -1,5 +1,6 @@
 module "lambda_api_gateway_getPerson" {
   source = "../../modules/aws/aws_lambda_api_server_integration/"
+  count  = var.create_lambda_api_gateway_getPerson ? 1 : 0
 
   lambda_function_file_path = "./AWS-Lambda-API-Gateway.py"
   lambda_function_runtime   = "python3.11"
---
+++ Home-Lab/prod/aws/variables.tf
@@ -0,0 +1,4 @@
+variable "create_lambda_api_gateway_getPerson" {
+  type    = bool
+  default = false
+}
