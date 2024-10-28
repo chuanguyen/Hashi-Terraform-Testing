45192c391c24bad8162c9b60b331f2c3609e7a51
Calvin Hua-Nguyen <github.vcs.code@m.chuanguyen.ca>
2024-10-28 12:08:56 -0700

    Migrate Proxmox resources into the module I created: - Moved the resources, so didn't need to recreate

--- Home-Lab/dev/proxmox/main.tf
+++ Home-Lab/dev/proxmox/main.tf
@@ -1,104 +1,67 @@
-resource "proxmox_vm_qemu" "k3s_masters" {
-  count       = var.master_num_nodes
-  name        = "k3s-master-${count.index}"
-  target_node = var.pm_node_name
+module "k3s_masters" {
+  source = "../../modules/proxmox/proxmox_create_vm_from_template"
+
+  pm_vm_count         = var.master_num_nodes
+  pm_vm_name          = "k3s-master"
+  pm_target_node_name = var.pm_node_name
   # When cloning, ensure the disk size matches the disk in the template
   # Otherwise, disk won't clone properly
-  clone      = var.master_template_vm_name
-  full_clone = var.master_full_clone
-  os_type    = "cloud-init"
-  agent      = var.master_enable_qemu_agent
-  memory     = var.master_mem
-  cores      = var.master_cpu_cores
+  pm_vm_template_name     = var.master_template_vm_name
+  pm_vm_full_clone        = var.master_full_clone
+  pm_vm_enable_qemu_agent = var.master_enable_qemu_agent
+  pm_vm_mem               = var.master_mem
+  pm_vm_cpu_cores         = var.master_cpu_cores
 
   # Cloud-init config
-  ciuser     = var.master_ciuser
-  cipassword = var.master_cipassword
+  pm_vm_ciuser     = var.master_ciuser
+  pm_vm_cipassword = var.master_cipassword
 
-  ipconfig0 = "ip=dhcp"
-
-  disk {
-    slot      = "virtio0"
-    cache     = "none"
-    replicate = false
-    size      = var.master_disk_size
-    type      = var.master_disk_type
-    storage   = var.master_disk_location
-  }
-
-  disk {
-    slot    = "ide0"
-    type    = "cloudinit"
-    storage = var.master_disk_location
-  }
-
-  lifecycle {
-    ignore_changes = [
-      ciuser,
-      network,
-      disk
-    ]
-  }
+  pm_vm_disks = [
+    {
+      slot      = "virtio0"
+      cache     = "none"
+      replicate = false
+      size      = var.master_disk_size
+      type      = var.master_disk_type
+      storage   = var.master_disk_location
+    }
+  ]
 }
 
-resource "proxmox_vm_qemu" "k3s_workers" {
-  count       = var.worker_num_nodes
-  name        = "k3s-worker-${count.index}"
-  target_node = var.pm_node_name
+module "k3s_workers" {
+  source = "../../modules/proxmox/proxmox_create_vm_from_template"
+
+  pm_vm_count         = var.worker_num_nodes
+  pm_vm_name          = "k3s-worker"
+  pm_target_node_name = var.pm_node_name
   # When cloning, ensure the disk size matches the disk in the template
   # Otherwise, disk won't clone properly
-  clone      = var.worker_template_vm_name
-  full_clone = var.worker_full_clone
-  os_type    = "cloud-init"
-  agent      = var.worker_enable_qemu_agent
-  memory     = var.worker_mem
-  cores      = var.worker_cpu_cores
+  pm_vm_template_name     = var.worker_template_vm_name
+  pm_vm_full_clone        = var.worker_full_clone
+  pm_vm_enable_qemu_agent = var.worker_enable_qemu_agent
+  pm_vm_mem               = var.worker_mem
+  pm_vm_cpu_cores         = var.worker_cpu_cores
 
   # Cloud-init config
-  ciuser     = var.worker_ciuser
-  cipassword = var.worker_cipassword
+  pm_vm_ciuser     = var.worker_ciuser
+  pm_vm_cipassword = var.worker_cipassword
 
-  ipconfig0 = "ip=dhcp"
+  pm_vm_disks = [
+    {
+      slot      = "virtio0"
+      cache     = "none"
+      replicate = false
+      size      = var.worker_disk_size
+      type      = var.worker_disk_type
+      storage   = var.worker_disk_location
+    }
+  ]
 
-  disk {
-    slot      = "virtio0"
-    cache     = "none"
-    replicate = false
-    size      = var.worker_disk_size
-    type      = var.worker_disk_type
-    storage   = var.worker_disk_location
-  }
-
-  disk {
-    slot    = "ide0"
-    type    = "cloudinit"
-    storage = var.worker_disk_location
-  }
-
-  lifecycle {
-    ignore_changes = [
-      ciuser,
-      network,
-      disk
-    ]
-  }
-
-  depends_on = [proxmox_vm_qemu.k3s_masters]
+  depends_on = [module.k3s_masters]
 }
 
-# Iterates through created Proxmox VMs
-# Maps node IP to the node name
+# Create variables for the node to IP mapping
 locals {
-  master_node_ips = [for master_node in proxmox_vm_qemu.k3s_masters :
-    {
-      master_name = master_node.name
-      master_ip   = master_node.default_ipv4_address
-    }
-  ]
-  worker_node_ips = [for worker_node in proxmox_vm_qemu.k3s_workers :
-    {
-      worker_name = worker_node.name
-      worker_ip   = worker_node.default_ipv4_address
-    }
-  ]
+  master_node_ips = module.k3s_masters.pm_vm_outputs
+  worker_node_ips = module.k3s_workers.pm_vm_outputs
 }
--- Home-Lab/dev/proxmox/outputs.tf
+++ Home-Lab/dev/proxmox/outputs.tf
@@ -2,6 +2,6 @@ output "master_ips" {
   value = local.master_node_ips
 }
 
-output "node_ips" {
+output "worker_ips" {
   value = local.worker_node_ips
 }
